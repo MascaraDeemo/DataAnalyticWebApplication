a90302c6c10623f0df23332d60d60e7f62483458
MascaraDeemo <q924714879@gmail.com>
2018-05-25 22:17:39 +1000

    mongodb connection

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 var express  = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var session = require('express-session');
 
 var app = express();
 
-app.set('view engine', 'pug');
+// mongodb connection
+mongoose.connect("mongodb:localhost:27017/WikiLatic");
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
 
-app.get('/', function(req, res){
-    res.render('landingpage');
-});
+
+//parse incoming requests
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extend: false}));
+
+//static files from public
+app.use(express.static(__dirname + '/public'));
+
+//view engine with pug
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+//use routes
+var regist = require('./app/routes/registrationPage.routes');
+app.use('/', routes);
+
+
+
 app.listen(3000, function () {
     console.log("application running on Port 3000");
 })
