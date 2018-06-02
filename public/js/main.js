78a085d32184ba561d9157b20a24a9492adfe318
Zhixia Zhou <zhixia.zhou@connet.qut.edu.au>
2018-06-02 10:57:09 +1000

    overall functions all finished

--- public/js/main.js
+++ public/js/main.js
@@ -3,17 +3,25 @@ google.charts.load('current', {packages: ['corechart']});
 var optionsBar = {'title':"Distribution of different user type in each year ",
     'width':400,
     'height':300};
+var optionsPie = {'title':"Percentage of different user type",
+    'width':400,
+    'height':300};
 var data;
 
 function drawPie(){
+    var anonNum = 0, botNum = 0,adminNum = 0,userNum = 0;
+
     graphData = new google.visualization.DataTable();
-    graphData.addColumn('string', 'Element');
+    graphData.addColumn('string', 'Year');
     graphData.addColumn('number', 'Percentage');
-    $.each(data, function(key, val) {
-        graphData.addRow([key, val]);
-    })
+    $.each(data, function(index, val) {
+        anonNum = anonNum +val['anon'];
+        botNum = botNum +val['bot'];
+        adminNum = adminNum +val['admin'];
+        userNum = userNum + val['user'];
+    });
     var chart = new google.visualization.PieChart($("#myChart")[0]);
-    chart.draw(graphData, options);
+    chart.draw(graphData, optionsPie);
 }
 
 function drawBar(){
@@ -25,9 +33,9 @@ function drawBar(){
     graphData.addColumn('number','Admin');
     graphData.addColumn('number', 'Users');
     $.each(data, function(index, val) {
-        graphData.addRow([val['Year'],val['Anon'],val['Bot'],val['Admin'],val['Users']]);
+        graphData.addRow([val[]]);
     })
-    var chart = new google.visualization.BarChart($("#Chart")[0]);
+    var chart = new google.visualization.PieChart($("#myChart")[0]);
     chart.draw(graphData, optionsBar);
 }
 
